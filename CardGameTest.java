44a65d0b50675acccbe4b9e4818ae4704c27c261
JayantC31 <jayant.chawla31@gmail.com>
2023-11-08 16:13:35 +0000

    Commit 4 added gametest

---
+++ CardGameTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardGameTest {
+
+
+    @Test
+    void userInputs() {
+        // Test the User Inputs
+    }
+}
